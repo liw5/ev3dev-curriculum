9eb3fceb5c5328f6f992cf69bbfefa2c7ca56596
wangj13 <wangj13@rose-hulman.edu>
2018-01-31 16:16:53 -0500

    m2

--- sandbox/Wenxing Li/motors/m2_drive_encoders.py
+++ sandbox/Wenxing Li/motors/m2_drive_encoders.py
@@ -4,13 +4,15 @@ This module lets you practice using the encoder to determine distances while blo
 
 You will now use a run_to_rel_pos command to implement the action drive inches action.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Jixi Wang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
+# DONE: 2. Copy the contents of your m1_drive_timed.py and paste that text into
+#  this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
-# TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
+# Todo: 3. Add a beep after the drive motors stop (see code below).  Test your
+#  code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
 # TODO: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
@@ -44,4 +46,46 @@ Authors: David Fisher and PUT_YOUR_NAME_HERE.
 #
 # Observations you should make, run_to_rel_pos is easier to use since it uses encoders that are independent of speed.
 
+import ev3dev.ev3 as ev3
+import time
 
+
+def main():
+    print("--------------------------------------------")
+    print("  Timed Driving")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Drive using encoders").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    time_s = 1  # Any value other than 0.
+    while time_s != 0:
+
+        position_sp = int(input("distance to travel (inches): "))*90
+        speed_sp = int(input("Enter a speed (0 to 900 dps):"))
+        stop_action = 'brake'
+        left_motor.run_to_rel_pos(
+            position_sp, speed_sp, stop_action)
+        right_motor.run_to_rel_pos(
+            position_sp, speed_sp, stop_action)
+        ev3.Sound.beep().wait()
+        ev3.Sound.beep().wait()
+        left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        ev3.Sound.beep().wait()
+
+
+
+
+
+
+    print("Goodbye!")
+    ev3.Sound.speak("Goodbye").wait()
+
+main()

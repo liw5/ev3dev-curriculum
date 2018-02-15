36110cd9fbba6494e560372606e9b4d187e1749d
liw5 <liw5@rose-hulman.edu>
2018-02-15 17:06:52 -0500

    project init

--- projects/pc_project.py
+++ projects/pc_project.py
@@ -12,145 +12,26 @@ def main():
     main_frame = ttk.Frame(root, padding=20, relief='raised')
     main_frame.grid()
 
-    left_speed_label = ttk.Label(main_frame, text="Left")
-    left_speed_label.grid(row=0, column=0)
-    left_speed_entry = ttk.Entry(main_frame, width=8)
-    left_speed_entry.insert(0, "600")
-    left_speed_entry.grid(row=1, column=0)
-
-    right_speed_label = ttk.Label(main_frame, text="Right")
-    right_speed_label.grid(row=0, column=2)
-    right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
-    right_speed_entry.insert(0, "600")
-    right_speed_entry.grid(row=1, column=2)
-
-    # DONE: 3. Implement the callbacks for the drive buttons. Set both the
-    # click and shortcut key callbacks.
-    #
-    # To help get you started the arm up and down buttons have been implemented.
-    # You need to implement the five drive buttons.  One has been writen below to help get you started but is commented
-    # out. You will need to change some_callback1 to some better name, then pattern match for other button / key combos.
-
-    forward_button = ttk.Button(main_frame, text="Forward")
-    forward_button.grid(row=2, column=1)
-    # forward_button and '<Up>' key is done for your here...
-    forward_button['command'] = lambda: forward(mqtt_client,
-                                                int(left_speed_entry.get()),
-                                                int(right_speed_entry.get()))
-    root.bind('<Up>', lambda event: forward(mqtt_client,
-                                            int(left_speed_entry.get()),
-                                            int(right_speed_entry.get())))
-
-    left_button = ttk.Button(main_frame, text="Left")
-    left_button.grid(row=3, column=0)
-    # left_button and '<Left>' key
-    left_button['command'] = lambda: left(mqtt_client,
-                                          int(left_speed_entry.get()))
-    root.bind('<Left>', lambda event: left(mqtt_client,
-                                           int(left_speed_entry.get())))
-
-    stop_button = ttk.Button(main_frame, text="Stop")
-    stop_button.grid(row=3, column=1)
-    # stop_button and '<space>' key (note, does not need left_speed_entry, right_speed_entry)
-    stop_button['command'] = lambda: stop(mqtt_client)
-    root.bind('<space>', lambda event: stop(mqtt_client))
-
-    right_button = ttk.Button(main_frame, text="Right")
-    right_button.grid(row=3, column=2)
-    # right_button and '<Right>' key
-    right_button['command'] = lambda: right(mqtt_client,
-                                            int(right_speed_entry.get()))
-    root.bind('<Right>', lambda event: right(mqtt_client,
-                                             int(right_speed_entry.get())))
-
-    back_button = ttk.Button(main_frame, text="Back")
-    back_button.grid(row=4, column=1)
-    # back_button and '<Down>' key
-    back_button['command'] = lambda: backward(mqtt_client,
-                                              int(left_speed_entry.get()),
-                                              int(right_speed_entry.get()))
-    root.bind('<Down>', lambda event: backward(mqtt_client,
-                                               int(left_speed_entry.get()),
-                                               int(right_speed_entry.get())))
-
     up_button = ttk.Button(main_frame, text="Up")
     up_button.grid(row=5, column=0)
     up_button['command'] = lambda: send_up(mqtt_client)
     root.bind('<u>', lambda event: send_up(mqtt_client))
 
-    down_button = ttk.Button(main_frame, text="Down")
-    down_button.grid(row=6, column=0)
-    down_button['command'] = lambda: send_down(mqtt_client)
-    root.bind('<j>', lambda event: send_down(mqtt_client))
 
-    # Buttons for quit and exit
-    q_button = ttk.Button(main_frame, text="Quit")
-    q_button.grid(row=5, column=2)
-    q_button['command'] = (lambda: quit_program(mqtt_client, False))
-
-    e_button = ttk.Button(main_frame, text="Exit")
-    e_button.grid(row=6, column=2)
-    e_button['command'] = (lambda: quit_program(mqtt_client, True))
 
     root.mainloop()
 
 
-# ----------------------------------------------------------------------
-# Tkinter callbacks
-# ----------------------------------------------------------------------
-# DONE: 4. Implement the functions for the drive button callbacks.
-
-def forward(mqtt_client, left_speed_entry,
-            right_speed_entry):
-    print('forward')
-    mqtt_client.send_message("drive_forward",
-                             [left_speed_entry, right_speed_entry])
 
 
-def left(mqtt_client, left_speed_entry):
-    print('left')
-    mqtt_client.send_message("turn_left", [left_speed_entry])
 
 
-def right(mqtt_client, right_speed_entry):
-    print('right')
-    mqtt_client.send_message("turn_right", [right_speed_entry])
-
-
-def stop(mqtt_client):
-    print('stop')
-    mqtt_client.send_message("stop")
-
-
-def backward(mqtt_client, left_speed_entry, right_speed_entry):
-    print('backward')
-    mqtt_client.send_message("drive_backward", [left_speed_entry,
-                                                right_speed_entry])
-
-
-# TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
-# Observations you should make, you did basically this same program using the IR Remote, but your computer can be a
-# remote control that can do A LOT more than an IR Remote.  We are just doing the basics here.
-
-
-# Arm command callbacks
 def send_up(mqtt_client):
     print("arm_up")
     mqtt_client.send_message("arm_up")
 
 
-def send_down(mqtt_client):
-    print("arm_down")
-    mqtt_client.send_message("arm_down")
 
-
-# Quit and Exit button callbacks
-def quit_program(mqtt_client, shutdown_ev3):
-    if shutdown_ev3:
-        print("shutdown")
-        mqtt_client.send_message("shutdown")
-    mqtt_client.close()
-    exit()
 
 
 # ----------------------------------------------------------------------

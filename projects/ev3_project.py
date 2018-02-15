ffdc12fc798a64a3f8452ed9c10e530dc18b6278
liw5 <liw5@rose-hulman.edu>
2018-02-15 16:35:09 -0500

    project init

--- projects/ev3_project.py
+++ projects/ev3_project.py
@@ -13,8 +13,6 @@ def main():
     robot = robo.Snatch3r()
     mqtt_client = com.MqttClient(robot)
     mqtt_client.connect_to_pc()
-    # mqtt_clieKnt.connect_to_pc("35.194.247.175")  # Off campus IP address
-    # of a GCP broker
-    robot.loop_forever()
+
 
 main()

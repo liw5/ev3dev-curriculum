2dc1a5a5f7b10066bc26974a5f893d571266210c
liw5 <liw5@rose-hulman.edu>
2018-02-15 16:04:08 -0500

    added pc_project in projects folder

---
+++ projects/ev3_project.py
@@ -0,0 +1,6 @@
+"""
+This project is a delivery robot that can follow a line and identify the
+color in front of it. If the color is correct, it'll drop the object.
+Otherwise it'll send back a message to the tkinter window, and the operator
+can choose whether to stay and wait, or to go back to the original position.
+"""

aecabd152fbb15766203e962e853c0b90cd62d1c
liw5 <liw5@rose-hulman.edu>
2018-02-15 16:06:31 -0500

    added pc_project in projects folder

--- projects/ev3_project.py
+++ projects/ev3_project.py
@@ -4,3 +4,17 @@ color in front of it. If the color is correct, it'll drop the object.
 Otherwise it'll send back a message to the tkinter window, and the operator
 can choose whether to stay and wait, or to go back to the original position.
 """
+
+import mqtt_remote_method_calls as com
+import robot_controller as robo
+
+
+def main():
+    robot = robo.Snatch3r()
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
+    # mqtt_clieKnt.connect_to_pc("35.194.247.175")  # Off campus IP address
+    # of a GCP broker
+    robot.loop_forever()
+
+main()
